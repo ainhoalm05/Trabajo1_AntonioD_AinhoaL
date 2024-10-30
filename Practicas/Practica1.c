6c5c90affa68825b9d8d34e9503a5437cefddfda
ADelgadoLuque <tonyluque2006@gmail.com>
2024-10-30 21:00:43 +0100

    Temperatura máxima y mínima creadas

--- Practicas/Practica1.c
+++ Practicas/Practica1.c
@@ -13,18 +13,29 @@ int main (){
 	char dias[7][10] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
 	float temperatura[7] ;
 	float promedio=0;
-	//float mayor=0;
-	//float menor=0;
+	float mayor;
+	float menor;
 	
 	for (int i=0; i <7; i++){
 		printf("Introduce la temperatura del %s\n",dias[i]);
 		scanf("%f", &temperatura[i]);
 		promedio+=temperatura[i];
-
+		
+		if(i==0){ //en el primer ciclo del bucle asignamos el valor de la temperatura a mayor y menor para compararlas con el resto de días y sacar la temperatura mínima y máxima
+		menor=temperatura[i];
+		}else if (mayor<temperatura[i]){ //si la temperatura es mayor que la guardada en la variable se le asigna el valor  a esta, si no no hace nada
+		mayor=temperatura[i];
+		}
+		if(i==0){
+		menor=temperatura[i];
+		}else if (menor>temperatura[i]){
+		menor=temperatura[i];
+		}
 	}
 	promedio=promedio/7;
 	printf("La temperatura promedio de la semana: %lfºC\n", promedio);
-
+	printf("Temperatura más alta: %fºC\n",mayor);
+	printf("Temperatura más baja: %fºC\n",menor);
 
 	return 0;
 }
