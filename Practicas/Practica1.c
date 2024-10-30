5da57aa9598c9aee920e0951716fc2152be5fc6a
ADelgadoLuque <tonyluque2006@gmail.com>
2024-10-30 21:41:21 +0100

    máximos y mínimos terminado

--- Practicas/Practica1.c
+++ Practicas/Practica1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /*
  *
  *	Authors: Ainhoa Leonor y Antonio Delgado
@@ -15,7 +14,8 @@ int main (){
 	float promedio=0;
 	float mayor;
 	float menor;
-	
+	int dia_mayor,dia_menor;
+
 	for (int i=0; i <7; i++){
 		printf("Introduce la temperatura del %s\n",dias[i]);
 		scanf("%f", &temperatura[i]);
@@ -23,19 +23,25 @@ int main (){
 		
 		if(i==0){ //en el primer ciclo del bucle asignamos el valor de la temperatura a mayor y menor para compararlas con el resto de días y sacar la temperatura mínima y máxima
 		menor=temperatura[i];
+		dia_mayor=i; //dia_mayor guarda la posición del array a la que apuntar luego en el final
+		
 		}else if (mayor<temperatura[i]){ //si la temperatura es mayor que la guardada en la variable se le asigna el valor  a esta, si no no hace nada
 		mayor=temperatura[i];
+		dia_mayor=i;
+		
 		}
 		if(i==0){
 		menor=temperatura[i];
+		dia_menor=i;
 		}else if (menor>temperatura[i]){
 		menor=temperatura[i];
+		dia_menor=i;
 		}
 	}
 	promedio=promedio/7;
 	printf("La temperatura promedio de la semana: %lfºC\n", promedio);
-	printf("Temperatura más alta: %fºC\n",mayor);
-	printf("Temperatura más baja: %fºC\n",menor);
+	printf("Temperatura más alta: %s %fºC\n",dias[dia_mayor],mayor);
+	printf("Temperatura más baja: %s %fºC\n",dias[dia_menor],menor);
 
 	return 0;
 }
