95d3379866bbc9533bba4d05c13272612c43c1f4
ainhoalm05 <ainhoaleomat@gmail.com>
2024-10-30 12:00:45 +0100

    Clase 30/10/2024

---
+++ Practicas/Practica1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ *	Authors: Ainhoa Leonor y Antonio Delgado
+ *
+ */
+
+#define LUNES 0
+
+int main (){
+
+	float dias[7][10] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+	int temperatura=0;
+
+	for (int i; i == 7; i++){
+		printf("Introduce la temperatura de todos los dias de la semana");
+		scanf("%d\n", dias[i]);
+	}
+	temperaturas[LUNES]
+
+
+	return 0;
+}
