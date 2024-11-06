564a43a4444f0044afed76e236a983dc7c91cc6c
Antonio Delgado Luque <tonyluque2006@gmail.com>
2024-11-06 11:13:17 +0100

    Ejercicio 2 resuelto

--- Practicas/Ejercicio2.c
+++ Practicas/Ejercicio2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /*
  *
  *	Authors: Ainhoa Leonor y Antonio Delgado
@@ -8,30 +8,35 @@
  */
 
 int main (){
-	
-int cantidad_palabras;
 
+	int cantidad_palabras;
+	char x;
 	printf("Introduce el número de palabras a analizar\n");
 	scanf("%d",&cantidad_palabras);
 	char palabra[cantidad_palabras][50];
 	for(int i=0;i<cantidad_palabras;i++){
 		printf("Introduce una palabra\n");
 		scanf("%s",palabra[i]);
-	
-	}
 
-	for (int i=0;i<cantidad_palabras;i++){
-	
-		for (int j=0;;j++){
-		
-			if (palabra[i][j]=='a'){
-			
-				continue;
-			}else{
-			printf("%s",palabra[i]);
-			}
+	}
+	for (int i=0;i<cantidad_palabras;i++){//bucle para las palabras
+
+		for (int j=0;j<strlen(palabra[i]);j++){//bucle para las letras de cada palabra
+		x=palabra[i][j];
+		if (x=='a'){
+		break;
 		}
+		}
+	if (x=='a'){
+	continue;}
+	printf("%s\n",palabra[i]);
+	
 	}
 
-	return 0;
+	}
+
+	
+
+
+return 0;
 }
