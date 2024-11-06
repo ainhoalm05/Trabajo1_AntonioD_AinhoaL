c1d58284812ed5f499da4f0816f9b2cd20d139d4
Diego Díaz Morata <diegodiazming@gmail.com>
2024-11-06 14:24:43 +0100

    Practica 3.2

--- Practicas/Practica3.c
+++ Practicas/Practica3.c
@@ -8,27 +8,43 @@
  *	Authors: Ainhoa Leonor y Antonio Delgado
  *
  */
+typedef struct{
+	char nombre[20];
+	int stock;
+}Producto;
 
 int main (){
-	int producto;
-	char nom_producto[CANT_PRODUC][20];
-	int stock[CANT_PRODUC];
+	Producto producto[CANT_PRODUC];
+	int cantidad;
+	char respuesta;
+	int adicion=0;
 
 	printf("¿Cuantos productos quieres gestionar?:\n");
-	scanf("%d", &producto);
-	
-	if (producto <= CANT_PRODUC){
-	for(int i=0; i < producto; i++){
-		printf("Introduce el nombre de cada producto:\n");
-		scanf("%s", nom_producto[i]);
-		printf("Introduce el stock de cada producto:\n");
-		scanf("%d", &stock[i]);
-		//stock+=nom_producto;
+	scanf("%d",&cantidad);
+
+	if (cantidad <= CANT_PRODUC){
+		for(int i=0; i < cantidad; i++){
+			printf("Introduce el nombre de cada producto:\n");
+			scanf("%s", producto[i].nombre);
+			printf("Introduce el stock de cada producto:\n");
+			scanf("%d", &producto[i].stock);
+
+
+		}
 	}
+	for (int i=0;i<cantidad;i++){
+		printf("Producto %d:%s - %d unidades\n",i+1,producto[i].nombre,producto[i].stock);
 	}
-	printf("%d\n", producto);
-	printf("%s\n", nom_producto[2]);
-	printf("%d\n", stock[2]);
-	
+
+	printf("¿Desea rebastecer algun producto? (si/no):\n");
+	scanf("%s", &respuesta);
+
+	if (respuesta == "si"){
+		printf("Ingrese el nombre del producto:\n");
+		scanf("%s",producto);
+		printf("Cantidad a añadir:\n");
+		scanf("%d",&adicion);
+		producto[].stock+=adicion
+	}
 	return 0;
 }
