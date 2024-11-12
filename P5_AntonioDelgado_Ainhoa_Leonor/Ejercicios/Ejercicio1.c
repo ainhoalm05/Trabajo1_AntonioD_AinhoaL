6c0ad8184a428d363bb39c2658196f00b241fac9
Antonio Delgado Luque <tonyluque2006@gmail.com>
2024-11-12 14:33:03 +0100

    fin de clase 12/11/24

--- P5_AntonioDelgado_Ainhoa_Leonor/Ejercicios/Ejercicio1.c
+++ P5_AntonioDelgado_Ainhoa_Leonor/Ejercicios/Ejercicio1.c
@@ -8,53 +8,29 @@
  */
 
 int main (){
-int cantidad;
-int continente[cantidad];
-//para cada dato hago un array con tamaño cantidad para poder añadir más productos usando un bucle más  adelante
-int sede[cantidad];
-int categoria[cantidad];
-int modelo[cantidad];
-int lote[cantidad];
+	int cantidad;
 
-int inventario[1][2][3][4][9]; //Necesito un array de 5 dimensiones para guardar todos los datos
+	int inventario[1][2][3][5][9]; //Necesito un array de 5 dimensiones para guardar todos los datos
 
-inventario[0][0][0][0][0]=50; //Primer Ejemplo
-inventario[1][1][1][2][1]=120; //Segundo Ejemplo
+	inventario[0][0][0][0][0]=50; //Primer Ejemplo
+	inventario[1][1][1][2][1]=120; //Segundo Ejemplo
 
-
-printf("¿Cuántos productos quieres añadir?");
-scanf("%d",&cantidad);
-for (int i=0;i<cantidad;i++){
-//Recogo todos los datos necesarios para registrar las unidades
-printf("Introduce el continente (0 América 1 Europa): \n");
-scanf("%d",&continente[i]);
-//Guardo los  datos en un array para luego imprimirlos todos de golpe
-printf("Introduce la sede (0,1 o 2): \n");
-scanf("%d",&sede[i]);
-
-printf("Introduce la categoría (0 para televisores, 1 para teléfonos, 2 para ordenadores o 3 para electrodomésticos: \n");
-scanf("%d",&categoria[i]);
-
-printf("Introduce el modelo (0 a 4): \n");
-scanf("%d",&modelo[i]);
-
-printf("Introduce el lote (0 a 9): \n");
-scanf("%d",&lote[i]);
+	inventario[0][2][0][0][7]=6;
+	inventario[0][0][1][1][4]=25;
+	inventario[1][1][2][2][8]=87;
+	inventario[1][2][3][3][9]=152;
+	inventario[1][0][2][4][0]=999;
 
 
-printf("Introduce las unidades: \n");
-scanf("%d",&inventario[continente[i]][sede[i]][categoria[i]][modelo[i]][lote[i]]);
-//Estoy usando los arrays continente[i] sede[i] etc para señalar al array inventario[] dónde tiene que guardar las unidades
-}
 
-//Imprimimos los dos ejemplos
-printf("Unidades Primer ejemplo: %d\n",inventario[0][0][0][0][0]); 
-printf("Unidades Segundo ejemplo: %d\n",inventario[1][1][1][2][1]);
+	//Imprimimos los dos ejemplos
+	printf("Unidades Primer ejemplo: %d\n",inventario[0][0][0][0][0]); 
+	printf("Unidades Segundo ejemplo: %d\n",inventario[1][1][1][2][1]);
 
-//Imprimimos todos los productos 
-for (int i=0;i<cantidad;i++){
-printf("Unidades Producto %d: %d\n",i,inventario[continente[i]][sede[i]][categoria[i]][modelo[i]][lote[i]]);
-}
-
+	printf("Producto 1: %d\n",inventario[0][2][0][0][7]);
+	printf("Producto 2: %d\n",inventario[0][0][1][1][4]);
+	printf("Producto 3: %d\n",inventario[1][1][2][2][8]);
+	printf("Producto 4: %d\n",inventario[1][2][3][3][9]);
+	printf("Producto 5: %d\n",inventario[1][0][2][4][0]);
 	return 0;
 }
